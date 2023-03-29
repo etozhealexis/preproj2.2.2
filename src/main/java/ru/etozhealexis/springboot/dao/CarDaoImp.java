3ab29fa99a57db17382a44a0395e9f51745f94fc
etozhealexis <alexisworksya@yandex.ru>
2023-03-30 01:24:16 +0300

    working fine

--- src/main/java/ru/etozhealexis/springboot/dao/CarDaoImp.java
+++ src/main/java/ru/etozhealexis/springboot/dao/CarDaoImp.java
@@ -2,6 +2,7 @@ package ru.etozhealexis.springboot.dao;
 
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import ru.etozhealexis.springboot.model.Car;
 
@@ -13,9 +14,16 @@ public class CarDaoImp implements CarDao {
     @Autowired
     private EntityManager entityManager;
 
+    @Value("${maxCar}")
+    private int maxCount;
+
     @SuppressWarnings("unchecked")
     public List<Car> showCars(int count) {
         List<Car> allCars = entityManager.createQuery("SELECT c FROM Car c").getResultList();
+
+        if (maxCount < count) {
+            count = maxCount;
+        }
 
         return allCars.stream()
                 .limit(count)
