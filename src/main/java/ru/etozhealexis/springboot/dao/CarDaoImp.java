c6e8236bfcfe9ede34017da9dc4bdaa5ecc5bdc4
etozhealexis <alexisworksya@yandex.ru>
2023-03-29 22:30:05 +0300

    hiber makes jokes on me

--- src/main/java/ru/etozhealexis/springboot/dao/CarDaoImp.java
+++ src/main/java/ru/etozhealexis/springboot/dao/CarDaoImp.java
@@ -3,12 +3,14 @@ package ru.etozhealexis.springboot.dao;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.etozhealexis.springboot.model.Car;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
+@Transactional
 public class CarDaoImp implements CarDao {
     private final SessionFactory sessionFactory;
 
@@ -19,12 +21,11 @@ public class CarDaoImp implements CarDao {
 
     @SuppressWarnings("unchecked")
     public List<Car> showCars(int count) {
-        List<Car> allCars = sessionFactory.getCurrentSession().createQuery("FROM Car")
-                .getResultList();
+        List<Car> allCars = sessionFactory.getCurrentSession().createQuery("FROM Car").getResultList();
 
-        for (Car car : allCars) {
-            System.out.println(car.getModel() + " " + car.getSeries());
-        }
+//        for (Car car : allCars) {
+//            System.out.println(car.getModel() + " " + car.getSeries());
+//        }
         return allCars.stream()
                 .limit(count)
                 .collect(Collectors.toList());
